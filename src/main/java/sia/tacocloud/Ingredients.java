ec1eef00cd69350134ac66c8ad8c7f0164937ff7
TedXpro <johannes.woldeyes@gmail.com>
2023-07-26 13:19:51 +0300

    Add README file

--- src/main/java/sia/tacocloud/Ingredients.java
+++ src/main/java/sia/tacocloud/Ingredients.java
@@ -3,6 +3,14 @@ package sia.tacocloud;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
+@Data
+@RequiredArgsConstructor
 public class Ingredients {
-    
+    private final String id;
+    private final String name;
+    private final Type type;
+
+    public static enum Type {
+        WRAP, PROTEINS, VEGIES, CHEESE, SAUCE
+    }
 }
